23d7ae7ceb9ef3364bb7f4254f136935bdac653f
csgrabt <gy.cseko@outlook.com>
2021-03-16 19:12:59 +0100

    A Kutyák feladat megoldása, második rész:
    Kennel osztály másik 3 metódusa.

--- src/main/java/hu/nive/ujratervezes/zarovizsga/kennel/Kennel.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/kennel/Kennel.java
@@ -7,7 +7,7 @@ public class Kennel {
     List<Dog> dogs = new ArrayList<>();
 
     public List<Dog> getDogs() {
-        return dogs;
+        return new ArrayList<>(dogs);
     }
 
     public void addDog(Dog dog) {
@@ -22,5 +22,42 @@ public class Kennel {
         }
     }
 
+    public Dog findByName(String name) {
+        for (Dog item : dogs) {
+            if (item.getName().equals(name)) {
+
+                return item;
+            }
+        }
+        throw new IllegalArgumentException("WWe do not have the dog which name: " + name);
+    }
+
+    public void playWith(String name, int hours) {
+
+        findByName(name).play(hours);
+
+    }
+
+
+    public List<String> getHappyDogNames(int minHappiness) {
+        List<String> happyDogs = new ArrayList<>();
+
+        for (Dog item : dogs
+        ) {
+            if (item.getHappiness() > minHappiness) {
+                happyDogs.add(item.getName());
+            }
+        }
+        weDoNotHaveHappyDog(happyDogs);
+
+        return happyDogs;
+    }
+
+
+    private void weDoNotHaveHappyDog(List<String> happyDogs) {
+        if (happyDogs.isEmpty()) {
+            throw new IllegalArgumentException("We do not have Happy dogs!");
+        }
+    }
 
 }
