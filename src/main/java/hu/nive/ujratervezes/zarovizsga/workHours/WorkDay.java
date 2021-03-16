8b00da41e532bc31a75953171f3e1b86331f2fa8
csgrabt <gy.cseko@outlook.com>
2021-03-16 18:06:54 +0100

    WorkDay feladat megoldása 18:06

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/workHours/WorkDay.java
@@ -0,0 +1,25 @@
+package hu.nive.ujratervezes.zarovizsga.workHours;
+
+public class WorkDay {
+    private String name;
+    private String date;
+    private int hours;
+
+    public WorkDay(String name, String date, int hours) {
+        this.name = name;
+        this.date = date;
+        this.hours = hours;
+    }
+
+    public WorkDay() {
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    @Override
+    public String toString() {
+        return  name + ": " + date;
+    }
+}
---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/workHours/WorkHours.java
@@ -0,0 +1,63 @@
+package hu.nive.ujratervezes.zarovizsga.workHours;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+public class WorkHours {
+
+    private static final String REGEX = ",";
+
+    public String minWork(String path) {
+        Path path1 = Path.of(path);
+        WorkDay workDay = new WorkDay();
+
+        try (BufferedReader br = Files.newBufferedReader(path1)) {
+            int counter = 0;
+            workDay = dataProcessing(workDay, br, counter);
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException("File not found!", ioe);
+        }
+
+
+        return workDay.toString();
+    }
+
+    private WorkDay dataProcessing(WorkDay workDay, BufferedReader br, int counter) throws IOException {
+        String line;
+        while ((line = br.readLine()) != null) {
+
+            String[] array = line.split(REGEX);
+
+            workDay = firstLine(workDay, counter, array);
+
+            workDay = afterFirstLine(workDay, counter, array);
+
+            counter++;
+        }
+
+        return workDay;
+    }
+
+
+    private WorkDay firstLine(WorkDay workDay, int counter, String[] array) {
+        if (counter == 0) {
+            workDay = getWorkDay(array);
+        }
+        return workDay;
+    }
+
+
+    private WorkDay afterFirstLine(WorkDay workDay, int counter, String[] array) {
+        if (workDay.getHours() > Integer.parseInt(array[1]) && counter > 0) {
+            workDay = getWorkDay(array);
+        }
+        return workDay;
+    }
+
+    private WorkDay getWorkDay(String[] array) {
+        return new WorkDay(array[0], array[2], Integer.parseInt(array[1]));
+    }
+}
